c59550bbea6d1050bcad2dfa941919efd5269756
Paiva <danpaiva@live.com>
2025-09-20 15:31:51 -0300

    feat: add files

---
+++ PizzaApi/PizzaApi/Data/PizzaDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaApi.Models;
+
+namespace PizzaApi.Data;
+
+class PizzaDb : DbContext
+{
+    public PizzaDb(DbContextOptions<PizzaDb> options) : base(options) { }
+    public DbSet<Pizza> Pizzas { get; set; } = null!;
+}
